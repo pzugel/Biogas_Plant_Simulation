d8e7a2c3103285c278ac80e655e3e12c7df53b49
pzugel <pzuegel@googlemail.com>
2021-08-16 14:26:48 +0200

    Updating VRL Project file

--- VRL/groovy_code_elements/projectDir.java
+++ VRL/groovy_code_elements/projectDir.java
@@ -7,21 +7,21 @@ public class projectDir implements java.io.Serializable {
   private static final long serialVersionUID=1L;
   public static String dir;
     
-  @MethodInfo(name="getDirectory", valueName="Directory",valueStyle="silent", hide=false)
+  @MethodInfo(name="getDirectory", 
+		valueName="Directory",
+		valueStyle="silent", 
+		hide=false)
   public Path projectDirectory(){
     File scriptDir = new File(getClass().protectionDomain.codeSource.location.path);
     dir = scriptDir.toString();
     String os = System.getProperty("os.name");
-    int end = dir.indexOf(".vrlp");
-    
+    int end = dir.indexOf(".vrlp");   
     int start;
     
     if(os.contains("Windows")){
-      String home = System.getProperty("user.home");
-      String homeReplace = home.replaceAll(":", "");
-      start = dir.lastIndexOf(homeReplace);
-      dir = dir.substring(start,end+5);
-      dir = dir.replace(homeReplace, home);
+      start = dir.indexOf("Drive_");
+      String drive = dir.substring(start+6, start+7);
+      dir = drive + ":\\" + dir.substring(start+8,end+5);
     } else if(os.contains("Linux")){
       start = dir.lastIndexOf("/home");
       dir = dir.substring(start,end+5);
@@ -33,7 +33,9 @@ public class projectDir implements java.io.Serializable {
 
   @MethodInfo(name="manualDirectory", valueName="Directory",valueStyle="silent", hide=true)
   public Path manualDirectory(
-    @ParamInfo(name="Project Directory", style="load-dialog", options="endings=[\".vrlp\"]; description=\"Path to project\"") String dir){
+    @ParamInfo(name="Project Directory", 
+			style="load-dialog", 
+			options="endings=[\".vrlp\"]; description=\"Path to project\"") String dir){
       System.out.println("dir: " + dir);
       return Paths.get(dir); 
   }
